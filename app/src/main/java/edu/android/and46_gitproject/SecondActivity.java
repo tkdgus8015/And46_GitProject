6eca2e32ef1e09004f214fd0ec3db2a6590c55d6
SangHyunKim <tkdgus8015@naver.com>
2018-10-19 15:25:07 +0900

    버튼 OnClick추가

--- app/src/main/java/edu/android/and46_gitproject/SecondActivity.java
+++ app/src/main/java/edu/android/and46_gitproject/SecondActivity.java
@@ -2,6 +2,7 @@ package edu.android.and46_gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
@@ -12,5 +13,12 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
